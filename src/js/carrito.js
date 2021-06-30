7b95aaef17219a2bde0330cb13c9beda747ab03c
danux99 <danix08@live.com>
2021-06-30 17:00:16 -0300

    funcion agregar al carrito

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -1,24 +1,39 @@
-class Carrito{
-    
+class Carrito {
+
     //agregar el producto al carrito
-    comprarProducto(e){
+    comprarProducto(e) {
         e.preventDefault();
-        if(e.target.classList.contains('agregar-carrito')){
+        if (e.target.classList.contains('agregar-carrito')) {
             const producto = e.target.parentElement.parentElement;
-            // this.leerDatosProducto(producto);
-            console.log(producto);
+            this.leerDatosProducto(producto);
+            // console.log(producto);
         }
+    }
+
+    leerDatosProducto(producto) {
+        const infoProducto = {
+            imagen: producto.querySelector('img').src,
+            titulo: producto.querySelector('h5').textContent,
+            precio: producto.querySelector('.precio span').textContent,
+            id: producto.querySelector('a').getAttribute('data-id'),
+            cantidad: 1
+        }
+        this.insertarCarrito(infoProducto);
+    }
+
+    insertarCarrito(producto) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            <img src="${producto.imagen}" width=100>
+        </td>
+        <td>${producto.titulo}</td>
+        <td>${producto.precio}</td>
+        <td>
+            <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
+        </td>
+        `;
+        listaProductos.appendChild(row);
     }
 }
 
-function sumarDetalles(){
-
-}
-
-function mostrarDetalles(){
-
-}
-
-function agregarIva(){
-
-}
