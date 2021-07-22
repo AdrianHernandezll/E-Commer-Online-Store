52609a74edf1e887a9d928ab1405c2bda85c886b
danux99 <danix08@live.com>
2021-07-21 23:49:14 -0300

    agregando api dolar

--- src/js/compra.js
+++ src/js/compra.js
@@ -3,7 +3,8 @@ const carrito = document.getElementById('carrito');
 const productos = document.getElementById('lista-productos');
 const listaProductos = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
-const procesarPedidoBtn = document.getElementById('procesar-pedido')
+const procesarPedidoBtn = document.getElementById('procesar-pedido');
+
 
 cargarEventos();
 
@@ -19,3 +20,38 @@ function cargarEventos() {
     //Enviar pedido a otra pagina
     procesarPedidoBtn.addEventListener('click', (e) => { carro.procesarPedido(e) });
 }
+
+$(document).ready(function () {
+    const APIURL = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
+
+
+    $.ajax({
+        method: "GET",
+        url: APIURL,
+        dataType: "json",
+        success: function (data) {
+            // console.log("Data que retorna el dolar :");
+            // console.log(data);
+            myArray = data[1].casa;
+            myArray2 = data[0].casa;
+            const row = `
+            <tr>
+                <td>${myArray.nombre}</td>
+                <td>${myArray.compra}</td>
+                <td>${myArray.venta}</td>
+            </tr>
+            `;
+            $("#dolar>tbody").append(row);
+
+            const row2 = `
+            <tr>
+                <td>${myArray2.nombre}</td>
+                <td>${myArray2.compra}</td>
+                <td>${myArray2.venta}</td>
+            </tr>
+            `;
+            $("#dolar>tbody").append(row2);
+        }
+    });
+
+});
